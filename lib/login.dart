d8acbbe7a235d62b771000c68ab6febc9ad5ddfc
zain <patniz486@gmail.com>
2024-03-12 19:55:00 +0500

    decorated textfields

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 // import 'package:flutter/widgets.dart';
 // import 'package:flutter/rendering.dart';
@@ -25,7 +26,6 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: const Colors.white(60, 214, 211, 211),
       backgroundColor: Colors.white,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -50,175 +50,178 @@ class _LoginState extends State<Login> {
               ),
             ),
           ),
-          const SizedBox(height: 5),
-          SingleChildScrollView(
-            child: Container(
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(50),
-                      topRight: Radius.circular(50)),
-                  color: Colors.black),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(25),
-                    child: Form(
-                      key: formData,
-                      child: Column(
-                        children: [
-                          //****name*****
-                          Container(
-                            child: TextFormField(
-                              decoration: const InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  hintText: 'Enter your Name',
-                                  hintStyle:
-                                      TextStyle(fontWeight: FontWeight.normal)),
-                              controller: nameController,
-                              validator: (String? value) {
-                                if (value == null || value.isEmpty) {
-                                  return "Name field cannot be empty";
-                                }
+          const SizedBox(height: 20),
+          Container(
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50)),
+                color: Colors.black),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Form(
+                    key: formData,
+                    child: Column(
+                      children: [
+                        //****name*****
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter your Name',
+                              hintStyle: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.white)),
+                          controller: nameController,
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return "Name field cannot be empty";
+                            }
 
-                                if (value.contains('@') ||
-                                    value.contains('.com')) {
-                                  return "Invalid Name";
-                                }
-                                // Check if the string contains an integer using a regular expression
-                                if (RegExp(r'\d').hasMatch(value)) {
-                                  return 'Try removing integer';
-                                }
-                                return null;
-                              },
-                            ),
-                          ),
-                          const SizedBox(height: 30),
-                          //******email*****
-                          TextFormField(
-                            decoration: const InputDecoration(
+                            if (value.contains('@') || value.contains('.com')) {
+                              return "Invalid Name";
+                            }
+                            // Check if the string contains an integer using a regular expression
+                            if (RegExp(r'\d').hasMatch(value)) {
+                              return 'Try removing integer';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 30),
+                        //******email*****
+                        TextFormField(
+                          decoration: const InputDecoration(
                               border: OutlineInputBorder(),
                               hintText: 'Enter your Email',
-                            ),
-                            controller: emailController,
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return "Email field cannot be empty";
-                              }
-                              if (!value.contains('@') ||
-                                  !value.contains('.com')) {
-                                return "Invalid email address";
-                              }
-                              return null;
-                            },
-                          ),
+                              hintStyle: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.white)),
+                          controller: emailController,
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return "Email field cannot be empty";
+                            }
+                            if (!value.contains('@') ||
+                                !value.contains('.com')) {
+                              return "Invalid email address";
+                            }
+                            return null;
+                          },
+                        ),
 
-                          //****age*****
-                          const SizedBox(height: 30),
-                          TextFormField(
-                            decoration: const InputDecoration(
+                        //****age*****
+                        const SizedBox(height: 30),
+                        TextFormField(
+                          decoration: const InputDecoration(
                               border: OutlineInputBorder(),
                               hintText: 'Enter your Age',
-                            ),
-                            keyboardType: TextInputType.number,
-                            controller: ageController,
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return "Enter your Age";
-                              }
-                              if (value.contains('@') ||
-                                  value.contains('.com')) {
-                                return "Invalid email address";
-                              }
-                              return null;
-                            },
-                          ),
-                          const SizedBox(height: 30),
-                          // Gender dropdown
-                          DropdownButtonFormField<String>(
-                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.white)),
+                          keyboardType: TextInputType.number,
+                          controller: ageController,
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return "Enter your Age";
+                            }
+                            if (value.contains('@') || value.contains('.com')) {
+                              return "Invalid email address";
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 30),
+                        // Gender dropdown
+                        DropdownButtonFormField<String>(
+                          decoration: const InputDecoration(
                               border: OutlineInputBorder(),
                               hintText: 'Please select your Gender',
-                            ),
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please select gender';
-                              }
-                              return null;
-                            },
-                            value: selectedGender,
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                selectedGender = newValue;
-                              });
-                            },
-                            items: <String>['Male', 'Female']
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                          ),
-                          const SizedBox(height: 30),
-                          //password
-                          TextFormField(
-                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.white)),
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please select gender';
+                            }
+                            return null;
+                          },
+                          value: selectedGender,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedGender = newValue;
+                            });
+                          },
+                          items: <String>['Male', 'Female']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                        const SizedBox(height: 30),
+                        //password
+                        TextFormField(
+                          decoration: const InputDecoration(
                               border: OutlineInputBorder(),
                               hintText: 'Enter your Password',
-                            ),
-                            controller: passwordController,
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return "Password field cannot be empty";
-                              }
-                              if (value.length <= 8) {
-                                return "Password length must be greater than 8";
-                              }
-                              return null;
-                            },
-                          ),
-                        ],
-                      ),
+                              hintStyle: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.white)),
+                          controller: passwordController,
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return "Password field cannot be empty";
+                            }
+                            if (value.length <= 8) {
+                              return "Password length must be greater than 8";
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 5),
-                  InkWell(
-                    onTap: () async {
-                      SharedPreferences sp =
-                          await SharedPreferences.getInstance();
-                      sp.setString('name', nameController.text.toString());
-                      sp.setString('email', emailController.text.toString());
-                      sp.setString('gender', selectedGender.toString());
-                      sp.setString('age', ageController.text.toString());
-                      sp.setBool('isLogin', true);
+                ),
+                const SizedBox(height: 5),
+                InkWell(
+                  onTap: () async {
+                    SharedPreferences sp =
+                        await SharedPreferences.getInstance();
+                    sp.setString('name', nameController.text.toString());
+                    sp.setString('email', emailController.text.toString());
+                    sp.setString('gender', selectedGender.toString());
+                    sp.setString('age', ageController.text.toString());
+                    sp.setBool('isLogin', true);
 
-                      if (formData.currentState!.validate()) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Info()));
-                      }
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.indigo),
-                        height: 50,
-                        width: double.infinity,
-                        // color: Colors.black,
-                        child: const Center(
-                          child: Text(
-                            'Login',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
+                    if (formData.currentState!.validate()) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Info()));
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.indigo),
+                      height: 50,
+                      width: double.infinity,
+                      // color: Colors.black,
+                      child: const Center(
+                        child: Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
                         ),
                       ),
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
           ),
         ],
